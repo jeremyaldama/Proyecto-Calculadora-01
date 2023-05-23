e35df28bbdd5cae178a2d4fd2311f6f56ffcd7ec
Jeremy Aldama <jeremyaldama23.2@gmail.com>
2023-05-22 23:43:44 -0500

    fix target event

--- js.js
+++ js.js
@@ -6,20 +6,23 @@ let sol;
 teclas.addEventListener("click", function (e) {
   const tecla = e.target;
   console.log(tecla);
-  const teclaValor = tecla.textContent;
 
-  if (teclaValor === "=") {
-    sol = Number.parseFloat(eval(resultado_cal)).toFixed(2);
-    resultado.value = sol;
-    resultado_cal = sol;
-  } else {
-    console.log(teclaValor);
-    resultado_cal += teclaValor;
-    resultado.value = resultado_cal;
-    console.log("cal: ", resultado_cal);
-    if (teclaValor === "C") {
-      resultado.value = "";
-      resultado_cal = "";
+  if (tecla.classList.contains("tecla")) {
+    const teclaValor = tecla.textContent;
+
+    if (teclaValor === "=") {
+      sol = Number.parseFloat(eval(resultado_cal)).toFixed(2);
+      resultado.value = sol;
+      resultado_cal = sol;
+    } else {
+      console.log(teclaValor);
+      resultado_cal += teclaValor;
+      resultado.value = resultado_cal;
+      console.log("cal: ", resultado_cal);
+      if (teclaValor === "C") {
+        resultado.value = "";
+        resultado_cal = "";
+      }
     }
   }
 });
