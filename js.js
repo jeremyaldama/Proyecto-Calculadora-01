4435031ccccb332dae0dc025892c5e026acc1d38
Jeremy Aldama <jeremyaldama23.2@gmail.com>
2023-05-22 23:27:42 -0500

    add operations functionality

--- js.js
+++ js.js
@@ -1,22 +1,25 @@
-const teclas = document.querySelector('.teclas');
-const resultado = document.querySelector('input');
-let resultado_cal = '';
+const teclas = document.querySelector(".teclas");
+const resultado = document.querySelector("input");
+let resultado_cal = "";
 let sol;
 
-teclas.addEventListener('click', function(e) {
-    const tecla = e.target;
-    console.log(tecla);
-    const teclaValor = tecla.textContent;
+teclas.addEventListener("click", function (e) {
+  const tecla = e.target;
+  console.log(tecla);
+  const teclaValor = tecla.textContent;
 
-    if (teclaValor === '=') {
-        sol = eval(resultado_cal);
-        resultado.value = sol;    
-    }
+  if (teclaValor === "=") {
+    sol = Number.parseFloat(eval(resultado_cal)).toFixed(2);
+    resultado.value = sol;
+    resultado_cal = sol;
+  } else {
     console.log(teclaValor);
     resultado_cal += teclaValor;
+    resultado.value = resultado_cal;
     console.log("cal: ", resultado_cal);
-    if (teclaValor === 'C') {
-        resultado.value = '';
-        resultado_cal = '';
+    if (teclaValor === "C") {
+      resultado.value = "";
+      resultado_cal = "";
     }
+  }
 });
