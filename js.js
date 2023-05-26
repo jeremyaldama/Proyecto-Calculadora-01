57dba65ef82f954f38840131135603196788ed3b
willydreamer <91001678+willydreamer@users.noreply.github.com>
2023-05-26 00:26:11 -0500

    Update js.js

--- js.js
+++ js.js
@@ -1,7 +1,18 @@
 const teclas = document.querySelector(".teclas");
 const resultado = document.querySelector("input");
+const toOperationsRecordSectionButton = document.getElementById('history-button');
 let resultado_cal = "";
 let sol;
+function chMode() {
+    mode.className == 'fa-solid fa-sun'?
+    mode.className = 'fa-solid fa-moon':
+    mode.className = 'fa-solid fa-sun';
+
+    let body = document.querySelector('body');
+    mode.classList.contains('fa-sun') ?
+    body.setAttribute('data-theme', 'dark') :
+    body.setAttribute('data-theme', 'light') ;
+}
 
 teclas.addEventListener("click", function (e) {
   const tecla = e.target;
@@ -11,18 +22,27 @@ teclas.addEventListener("click", function (e) {
     const teclaValor = tecla.textContent;
 
     if (teclaValor === "=") {
-      sol = Number.parseFloat(eval(resultado_cal)).toFixed(2);
-      resultado.value = sol;
-      resultado_cal = sol;
-    } else {
-      console.log(teclaValor);
-      resultado_cal += teclaValor;
-      resultado.value = resultado_cal;
-      console.log("cal: ", resultado_cal);
-      if (teclaValor === "C") {
-        resultado.value = "";
-        resultado_cal = "";
+        try {
+          sol = Number.parseFloat(eval(resultado_cal)).toFixed(2);
+          if (isNaN(sol)) {
+            throw new Error("Operación inválida");
+          }
+          resultado.value = sol;
+          resultado_cal = sol;
+        } catch (error) {
+          console.error(error);
+          resultado.value = "Error";
+        }
+      } else {
+        console.log(teclaValor);
+        resultado_cal += teclaValor;
+        resultado.value = resultado_cal;
+        console.log("cal: ", resultado_cal);
+        if (teclaValor === "C") {
+          resultado.value = "";
+          resultado_cal = "";
+        }
       }
-    }
   }
-});
+}
+);
